1091e0d56d8ce385609677a60e564d5c1eba9ae0
HynDuf <chauhuynhnoc@gmail.com>
2023-04-08 10:11:28 +0700

    Add test for /snippet/view

--- internal/models/users.go
+++ internal/models/users.go
@@ -9,6 +9,12 @@ import (
 	"time"
 )
 
+type UserModelInterface interface {
+	Insert(name, email, password string) error
+	Authenticate(email, password string) (int, error)
+	Exists(id int) (bool, error)
+}
+
 type User struct {
 	ID             int
 	Name           string
@@ -41,32 +47,32 @@ func (m *UserModel) Insert(name, email, password string) error {
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-    var id int
-    var hashedPassword []byte
-    stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
-    err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
-    if err != nil {
-        if errors.Is(err, sql.ErrNoRows) {
-            return 0, ErrInvalidCredentials
-        } else {
-            return 0, err
-        }
-    }
-    err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
-    if err != nil {
-        if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
-            return 0, ErrInvalidCredentials
-        } else {
-            return 0, err
-        }
-    }
+	var id int
+	var hashedPassword []byte
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
 	return id, nil
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
-    var exists bool
-    stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
+	var exists bool
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
 
-    err := m.DB.QueryRow(stmt, id).Scan(&exists)
-    return exists, err
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+	return exists, err
 }
