0b24069be0288538941bebdce7055b0f21ce79d7
HynDuf <chauhuynhnoc@gmail.com>
2023-04-08 09:44:51 +0700

    Add simple unit test

---
+++ cmd/web/templates_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"snippetbox.hynduf/internal/assert"
+	"testing"
+	"time"
+)
+
+func TestHumanDate(t *testing.T) {
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2023, 4, 8, 8, 52, 0, 0, time.UTC),
+			want: "08 Apr 2023 at 08:52",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2022, 3, 17, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "17 Mar 2022 at 09:15",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hd := humanDate(tt.tm)
+			assert.Equal(t, hd, tt.want)
+		})
+	}
+}
